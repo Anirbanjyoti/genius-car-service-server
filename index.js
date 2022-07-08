fbcc5ebf0e81dfd64bbf334d70c2643bfdb94116
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-07-08 17:54:19 +0530

    genius car server

---
+++ index.js
@@ -0,0 +1,119 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Verify JWToken
+function verifyJWT(req, res, next){
+        // sending token
+        const authHeader = req.headers.authorization;
+        if(!authHeader){
+          return res.status(401).send({message: 'Unauthorized Access'});
+        }
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRETE, (err, decoded)=>{
+            if(err){
+              return res.status(403).send({message: 'Forbidden access'})
+             }
+             console.log(`decoded`, decoded);
+             req.decoded = decoded;             
+        }) 
+        console.log('iNSIDE verifyJWT', authHeader);
+        next(); 
+}
+// Database connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w2lwko6.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+//
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client.db("geniusCar").collection("service");
+    const orderCollection = client.db("geniusCar").collection("order");
+
+    // login token API
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRETE, {
+        expiresIn: "1d",
+      });
+      res.send({ accessToken });
+    });
+    //  create API to get Multiple data
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+    //  create API to get single data
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+    // Post API-- to create / add service to all service
+    app.post("/service", async (req, res) => {
+      const newService = req.body;
+      const result = await serviceCollection.insertOne(newService);
+      res.send(result);
+    });
+    // get all Orders
+    app.get("/orders", verifyJWT,  async (req, res) => {
+      //  je shdhu mstro login korsa ache tar info onujayi tar ordergulo dekhabe.
+      const decodedEmail = req.decoded.email;
+      const email = req.query.email;
+      console.log(email);
+
+      if(email===decodedEmail){
+        const query = { email: email };
+        const cursor = orderCollection.find(query);
+        const orders = await cursor.toArray();
+        res.send(orders);
+      }
+      else{
+        res.status(403).send({message: 'Forbidden Access'});
+      }
+    });
+    // Post API-- to create / add Order to all OrderCollection
+    app.post("/order", async (req, res) => {
+      const newOrder = req.body;
+      const result = await orderCollection.insertOne(newOrder);
+      res.send(result);
+    });
+
+    // Post API-- to Delete service
+    app.delete("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+// api declare
+app.get("/", (req, res) => {
+  res.send("Running Genius Car Server");
+});
+
+// Listening port
+app.listen(port, () => {
+  console.log(`Listening to port`, port);
+});
